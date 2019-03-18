aa265ce417eb6926bc4c874de08887afd2ce6843
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-03-17 23:59:00 -0500

    some html and broken player cards

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+let mike = {
+    firstName: 'Mike',
+    lastName: 'Tyson',
+    nickName: 'Iron Mike',
+    healthPoints: 80,
+    attackPower: 8,
+    counterAttack: 5,
+    isPlayer: false
+};
+
+let ali = {
+    firstName: 'Muhammad',
+    lastName: 'Ali',
+    nickName: 'The Greatest',
+    healthPoints: 120,
+    attackPower: 5,
+    counterAttack: 5,
+    isPlayer: false
+};
+
+let mayweather = {
+    firstName: 'Floyd',
+    lastName: 'Mayweather',
+    nickName: 'Money Mayweather',
+    healthPoints: 110,
+    attackPower: 4,
+    counterAttack: 8,  
+    isPlayer: false 
+};
+
+let roy = {
+    firstName: 'Roy',
+    lastName: 'Jones',
+    nickName: 'Captain Hook',
+    healthPoints: 100,
+    attackPower: 6,
+    counterAttack: 8, 
+    isPlayer: false
+};
+
+let playerScore = 0;
+let opponentScore = 0;
