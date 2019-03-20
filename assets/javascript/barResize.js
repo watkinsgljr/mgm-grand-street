69d2688f0818679eb7ac0021da0f9581f0c79fc4
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-03-20 00:25:44 -0500

    set id in opjects and set object.assign to current player home stretch tomorrow

---
+++ assets/javascript/barResize.js
@@ -0,0 +1,18 @@
+// //HP bar logic
+// function barResize() {
+//     const hpBeg = player.healthPoints;
+//     let hpCurrent = player.healthPoints;
+//     hpBarLengthPerc = hpBeg / hpCurrent;
+//     $('#hp-bar').width((hpBarLengthPerc * 100) % );
+// }
+
+
+// function numOfVowels(string) {
+//    let counter = 0
+//    vowelArray = ["a","e", "i", "o", "u", "A","E", "I", "O", "U"]
+//    (i = 0; i < string.length; i++)
+//      if (indexOf(string[i]) != -1) {
+//         counter += 1;
+//      }
+    
+// }
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,54 @@
-let mike = {
+const mike = {
     firstName: 'Mike',
     lastName: 'Tyson',
     nickName: 'Iron Mike',
     healthPoints: 80,
-    attackPower: 8,
+    attackPower: 7,
     counterAttack: 5,
-    isPlayer: false
+    isPlayer: false,
+    currentOpponent: false,
+    opponentDefeated: false,
+    id: 'mike-card-holder'
 };
 
-let ali = {
+const ali = {
     firstName: 'Muhammad',
     lastName: 'Ali',
     nickName: 'The Greatest',
     healthPoints: 120,
     attackPower: 5,
     counterAttack: 5,
-    isPlayer: false
+    isPlayer: false,
+    currentOpponent: false,
+    opponentDefeated: false
 };
 
-let mayweather = {
+const mayweather = {
     firstName: 'Floyd',
     lastName: 'Mayweather',
     nickName: 'Money Mayweather',
     healthPoints: 110,
     attackPower: 4,
     counterAttack: 8,  
-    isPlayer: false 
+    isPlayer: false,
+    currentOpponent: false,
+    opponentDefeated: false 
 };
 
-let roy = {
+const roy = {
     firstName: 'Roy',
     lastName: 'Jones',
     nickName: 'Captain Hook',
     healthPoints: 100,
     attackPower: 6,
     counterAttack: 8, 
-    isPlayer: false
+    isPlayer: false,
+    currentOpponent: false,
+    opponentDefeated: false
 };
-
+let currentPlayer;
+let currentOpponent;
+let defeatedOpponents = [];
 let playerScore = 0;
 let opponentScore = 0;
 let playerSet = false;
@@ -46,24 +57,32 @@ const mikeCard = $('#mike-card-holder');
 const aliCard = $('#ali-card-holder');
 const mayweatherCard = $('#mayweather-card-holder');
 const royCard = $('#roy-card-holder');
+let punchPower = $('#player-punch-power');
+let counterPunch = $('opponent-counter-power');
 
  //Function upon selecting player
 
 mikeCard.click(function () {
     if (playerSet == false) {
         $(this).appendTo('#ring-area');
-        mike.isPlayer = true;
+        currentPlayer = Object.assign({}, mike);
         playerSet = true;
-        console.log(mike.isPlayer);
+        console.log(currentPlayer);
         console.log(playerSet);
         $(this).width('30%');
-        $('#welcome-text h1').text('Select Your First Opponent');       
+        $('#hp-bar').width('50%');
+        $('#welcome-text h1').text('Select Your First Opponent');  
+        loadDash(mike);     
     } else {
         $(this).appendTo('#ring-area');
         console.log(mike.isPlayer);
         console.log(playerSet);
+        opponentArray.append(mike.lastName);
+        console.log(mike.lastName);
+        mike.currentOpponent = true;
         $(this).width('30%');
         $('#welcome-text h1').text('Round 1: FIGHT');
+        loadDash(mike);
     }
  });
 
@@ -82,7 +101,7 @@ aliCard.click(function () {
         console.log(playerSet);
         $(this).width('30%');
         $('#welcome-text h1').text('Round 1: FIGHT');
-    }
+    } loadDash(ali);
  });
 
 mayweatherCard.click(function () {
@@ -100,7 +119,7 @@ mayweatherCard.click(function () {
         console.log(playerSet);
         $(this).width('30%');
         $('#welcome-text h1').text('Round 1: FIGHT');
-    }
+    } loadDash(mayweather);
  });
 
 royCard.click(function () {
@@ -118,17 +137,50 @@ royCard.click(function () {
         console.log(playerSet);
         $(this).width('30%');
         $('#welcome-text h1').text('Round 1: FIGHT');
-    }
+    } loadDash(roy);
  });
 
  
 
 //Load object into dashboard
+function loadDash (player) {
+    if (player.isPlayer === true) {
+       $('#player-punch-power').text(player.attackPower); 
+    } else $('#opponent-counter-power').text(player.counterAttack);
+};
+
 
 
  //HP bar logic
+ function resizeHPBar(player) {
+    const hpBeg = player.healthPoints;
+    let hpCurrent = player.healthPoints;
+    let hpBarLengthPerc = (hpBeg / hpCurrent) * 100;
+    $('#hp-bar').width((hpBarLengthPerc * 100));
+    return(hpBarLengthPerc);
+}
 
-//Beginning player and opponent selection
+//Fight - attack button on click
+
+function attack (player, opponent) {
+    opponent.heathPoints -= player.attackPoints;
+    player.punchPower += punchPower; 
+    resizeHPBar();
+    if (opponentHP > 0) {
+        player.healthPoints -= opponent.counterPower
+    } else {
+        opponentLose()
+    };
+    
+    resizeHPBar();
+    if (player.healthPoints <= 0) {
+        playerLose();
+    } 
+}
+
+
+//opponent lose
+// opponentLose 
 
 
 
