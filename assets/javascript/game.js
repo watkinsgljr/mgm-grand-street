6717059bd181ad5ce0da954f80b1eeb31e815725
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-03-20 20:37:03 -0500

    defeated functions workng and attack buttone

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ const mike = {
     lastName: 'Tyson',
     nickName: 'Iron Mike',
     healthPoints: 80,
+    basePower: 7,
     attackPower: 7,
     counterAttack: 5,
     isPlayer: false,
@@ -15,12 +16,14 @@ const ali = {
     firstName: 'Muhammad',
     lastName: 'Ali',
     nickName: 'The Greatest',
-    healthPoints: 120,
+    healthPoints: 80,
+    basePower: 7,
     attackPower: 5,
     counterAttack: 5,
     isPlayer: false,
     currentOpponent: false,
-    opponentDefeated: false
+    opponentDefeated: false,
+    id: 'ali-card-holder'
 };
 
 const mayweather = {
@@ -28,11 +31,13 @@ const mayweather = {
     lastName: 'Mayweather',
     nickName: 'Money Mayweather',
     healthPoints: 110,
+    basePower: 7,
     attackPower: 4,
     counterAttack: 8,  
     isPlayer: false,
     currentOpponent: false,
-    opponentDefeated: false 
+    opponentDefeated: false ,
+    id: 'mayweather-card-holder'
 };
 
 const roy = {
@@ -40,17 +45,19 @@ const roy = {
     lastName: 'Jones',
     nickName: 'Captain Hook',
     healthPoints: 100,
+    basePower: 7,
     attackPower: 6,
     counterAttack: 8, 
     isPlayer: false,
     currentOpponent: false,
-    opponentDefeated: false
+    opponentDefeated: false,
+    id: 'roy-card-holder'
 };
+
+// GLOBAL VARIABLES --------------------------------------------------------------------------------------------------------------
 let currentPlayer;
 let currentOpponent;
 let defeatedOpponents = [];
-let playerScore = 0;
-let opponentScore = 0;
 let playerSet = false;
 const boxerCard = $('.boxer-card-holder');
 const mikeCard = $('#mike-card-holder');
@@ -60,26 +67,25 @@ const royCard = $('#roy-card-holder');
 let punchPower = $('#player-punch-power');
 let counterPunch = $('opponent-counter-power');
 
- //Function upon selecting player
+ //Function upon selecting player --------------------------------------------------------------------------------------
+ // boxer click events -------------------------------------------------------------------------------------------------
 
 mikeCard.click(function () {
     if (playerSet == false) {
         $(this).appendTo('#ring-area');
         currentPlayer = Object.assign({}, mike);
+        console.log(currentPlayer)
         playerSet = true;
-        console.log(currentPlayer);
-        console.log(playerSet);
         $(this).width('30%');
-        $('#hp-bar').width('50%');
+        // $('#hp-bar').width('50%');
         $('#welcome-text h1').text('Select Your First Opponent');  
         loadDash(mike);     
+    } else if (mike.opponentDefeated == true) {
+        alert("Fighter has already been defeated.  Please select another boxer.")
     } else {
         $(this).appendTo('#ring-area');
-        console.log(mike.isPlayer);
-        console.log(playerSet);
-        opponentArray.append(mike.lastName);
-        console.log(mike.lastName);
-        mike.currentOpponent = true;
+        currentOpponent = Object.assign({}, mike);
+        console.log(currentOpponent)
         $(this).width('30%');
         $('#welcome-text h1').text('Round 1: FIGHT');
         loadDash(mike);
@@ -89,14 +95,18 @@ mikeCard.click(function () {
 aliCard.click(function () {
     if (playerSet == false) {
         $(this).appendTo('#ring-area');
+        currentPlayer = Object.assign({}, ali);
         ali.isPlayer = true;
         playerSet = true;
         console.log(ali.isPlayer);
         console.log(playerSet);
         $(this).width('30%');
         $('#welcome-text h1').text('Select Your First Opponent');       
+    } else if (ali.opponentDefeated == true) {
+        alert("Fighter has already been defeated.  Please select another boxer.")
     } else {
         $(this).appendTo('#ring-area');
+        currentOpponent = Object.assign({}, ali);
         console.log(ali.isPlayer);
         console.log(playerSet);
         $(this).width('30%');
@@ -107,14 +117,18 @@ aliCard.click(function () {
 mayweatherCard.click(function () {
     if (playerSet == false) {
         $(this).appendTo('#ring-area');
+        currentPlayer = Object.assign({}, mayweather);
         mayweather.isPlayer = true;
         playerSet = true;
         console.log(mayweather.isPlayer);
         console.log(playerSet);
         $(this).width('30%');
         $('#welcome-text h1').text('Select Your First Opponent');       
+    } else if (mayweather.opponentDefeated == true) {
+        alert("Fighter has already been defeated.  Please select another boxer.")
     } else {
         $(this).appendTo('#ring-area');
+        currentOpponent = Object.assign({}, mayweather);
         console.log(mayweather.isPlayer);
         console.log(playerSet);
         $(this).width('30%');
@@ -125,14 +139,18 @@ mayweatherCard.click(function () {
 royCard.click(function () {
     if (playerSet == false) {
         $(this).appendTo('#ring-area');
+        currentPlayer = Object.assign({}, roy);
         roy.isPlayer = true;
         playerSet = true;
         console.log(roy.isPlayer);
         console.log(playerSet);
         $(this).width('30%');
         $('#welcome-text h1').text('Select Your First Opponent');       
+    } else if (roy.opponentDefeated == true) {
+        alert("Fighter has already been defeated.  Please select another boxer.")
     } else {
         $(this).appendTo('#ring-area');
+        currentOpponent = Object.assign({}, roy);
         console.log(roy.isPlayer);
         console.log(playerSet);
         $(this).width('30%');
@@ -142,7 +160,8 @@ royCard.click(function () {
 
  
 
-//Load object into dashboard
+//Load object into dashboard ----------------------------------------------------------------------
+
 function loadDash (player) {
     if (player.isPlayer === true) {
        $('#player-punch-power').text(player.attackPower); 
@@ -151,36 +170,69 @@ function loadDash (player) {
 
 
 
- //HP bar logic
+ //HP bar logic---------------------------------------------------------------------------------------------
+
  function resizeHPBar(player) {
-    const hpBeg = player.healthPoints;
-    let hpCurrent = player.healthPoints;
+    const hpBeg = currentPlayer.healthPoints;
+    let hpCurrent = currentPlayer.healthPoints;
     let hpBarLengthPerc = (hpBeg / hpCurrent) * 100;
-    $('#hp-bar').width((hpBarLengthPerc * 100));
+    $('#hp-bar').width((hpBarLengthPerc * 100 + '%'));
     return(hpBarLengthPerc);
 }
 
-//Fight - attack button on click
+//Fight - attack button on click ------------------------------------------------------------------------------------
 
-function attack (player, opponent) {
-    opponent.heathPoints -= player.attackPoints;
-    player.punchPower += punchPower; 
-    resizeHPBar();
-    if (opponentHP > 0) {
-        player.healthPoints -= opponent.counterPower
+$('#attack').click(function attack () {
+    if (currentOpponent == null) {
+        alert("Select your next match.")
+
     } else {
-        opponentLose()
-    };
+
+    currentOpponent.healthPoints -= currentPlayer.attackPower;
+    currentPlayer.attackPower += currentPlayer.basePower;
+    loadDash(currentPlayer);
+    loadDash(currentOpponent);
+    console.log(currentPlayer.attackPower) 
+    console.log(currentOpponent.healthPoints)
     
     resizeHPBar();
-    if (player.healthPoints <= 0) {
-        playerLose();
-    } 
-}
+    } if (currentOpponent.healthPoints > 0) {
+        currentPlayer.healthPoints -= currentOpponent.counterAttack;
+        console.log(currentPlayer.healthPoints)
+    } else if (currentOpponent.healthPoints <= 0) {
+        opponentDefeated();
+    }; 
+}); 
+
+function opponentDefeated() {
+    $('#' + currentOpponent.id).appendTo($('#opponent-area'));
+    $('#' + currentOpponent.id).css("opacity", .4);
+    currentOpponent.opponentDefeated = true;
+    console.log(currentOpponent.opponentDefeated)
+    $('#' + currentOpponent.id).unbind("click");
+    defeatedOpponents.push(currentOpponent.lastName);
+    console.log(defeatedOpponents)
+    currentOpponent = null;
+    
+    console.log()
+    //grey out card with grey overlay...opponent.show
+};
 
 
-//opponent lose
-// opponentLose 
+
+
+
+
+
+
+
+
+
+//SOURCES----------------------------------------
+// https://codepen.io/susanwinters/pen/epLzRG
+
+// https://www.codeproject.com/Questions/736336/How-Can-I-Diable-Click-On-A-Div
+
 
 
 
